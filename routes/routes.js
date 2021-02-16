205ada212767aba5eef5e26fc6d4a1a54c650fa1
Mlipphardt <matthewlipphardt@gmail.com>
2021-02-15 21:23:07 -0500

    Added cors

--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,19 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/test", function (req, res) {
-  console.log("Testing");
-  res.send("Testing...");
+const fetch = require("node-fetch");
+
+router.get("/api/rhyme/:word", async (req, res) => {
+  let word = req.params.word;
+  let response = await fetch(`https://api.datamuse.com/words?rel_rhy=${word}`, {
+    headers: {
+      "Content-type": "Application/json",
+    },
+  });
+  let rhymes = await response.json();
+  res.json({
+    rhymes: rhymes,
+  });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require("express");
+var cors = require("cors");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 var routes = require("./routes/routes.js");
 
